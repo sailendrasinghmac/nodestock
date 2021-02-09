5947c5d157d61510fff2c6f96cafacf137ac3c23
Sailendra Singh <sailendrasinghmac@gmail.com>
2021-02-09 16:38:44 -0500

    added new codes for stock app

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path= require('path');
-
+const request = require('request');
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+//API key pk_036902fe354745c0920248587351ef4f
+//create call_api function
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_036902fe354745c0920248587351ef4f', { json:true}, (err, res, body) => {
+        if (err) {return console.log(err);}
+        if (res.statusCode === 200) {
+            //console.log(body);
+        finishedAPI(body);
+        };
+    });
+};
 
 
 //Set Handlebars Middleware
@@ -13,11 +29,35 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff!";
-//Set Handlebar routes
+//Set Handlebar index GET routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: otherstuff
-    });
+    call_api (function(doneAPI) {
+            res.render('home', {
+            stock: doneAPI
+        });
+    }, "fb");
+    
+});
+
+
+
+//Set Handlebar index POST routes
+app.post('/', function (req, res) {
+    call_api (function(doneAPI) {
+            //posted_stuff = req.body.stock_ticker;
+            res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
+    
+});
+
+
+
+
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 
